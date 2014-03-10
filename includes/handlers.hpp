c1ba1d8dede8b9e78672b93e1e18a007c23307a4
Marcin Zdun <mzdun@midnightbits.com>
2014-03-10 10:32:37 +0100

    Removing LNG from libenv (2)

--- includes/handlers.hpp
+++ includes/handlers.hpp
@@ -58,47 +58,6 @@ namespace FastCGI { namespace app
 	};
 	typedef std::shared_ptr<Handler> HandlerPtr;
 
-	class PageHandler: public Handler
-	{
-	protected:
-		virtual bool restrictedPage() { return true; }
-		virtual const char* getPageTitle(Request& request, PageTranslation& tr)
-		{
-			ContentPtr content = request.getContent();
-			if (content.get())
-				return content->getPageTitle(tr);
-			return nullptr;
-		}
-		std::string getTitle(Request& request, PageTranslation& tr)
-		{
-			std::string title = tr(lng::LNG_GLOBAL_SITENAME);
-			const char* page = getPageTitle(request, tr);
-			if (!page) return title;
-			title += " &raquo; ";
-			title += page;
-			return title;
-		} 
-		//rendering the page
-		virtual void prerender(const SessionPtr& session, Request& request, PageTranslation& tr) {}
-		virtual void title(const SessionPtr& session, Request& request, PageTranslation& tr);
-		virtual void header(const SessionPtr& session, Request& request, PageTranslation& tr);
-		virtual void headElement(const SessionPtr& session, Request& request, PageTranslation& tr);
-		virtual void buildTopMenu(TopMenu::TopBar& menu, const SessionPtr& session, Request& request, PageTranslation& tr);
-		virtual void bodyStart(const SessionPtr& session, Request& request, PageTranslation& tr);
-		virtual void render(const SessionPtr& session, Request& request, PageTranslation& tr)
-		{
-			ContentPtr content = request.getContent();
-			if (content.get())
-				content->render(session, request, tr);
-		}
-		virtual void footer(const SessionPtr& session, Request& request, PageTranslation& tr);
-		virtual void bodyEnd(const SessionPtr& session, Request& request, PageTranslation& tr);
-		virtual void topbarUI(const SessionPtr& session, Request& request, PageTranslation& tr);
-		virtual void postrender(const SessionPtr& session, Request& request, PageTranslation& tr) {}
-	public:
-		void visit(Request& request);
-	};
-
 	class RedirectUrlHandler: public Handler
 	{
 	protected:
